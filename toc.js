13c91f87a456b604ef7ce849caaaca22e6b134b6
eksperimental <eksperimental@users.noreply.github.com>
2015-01-02 15:34:11 +1100

    add  setting. README.md updated

--- toc.js
+++ toc.js
@@ -5,13 +5,14 @@
       noBackToTopLinks: false,
       title: '<i>Jump to...</i>',
       minimumHeaders: 3,
+      headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
       showEffect: 'show', // values: [show|slideDown|fadeIn|none]
       showSpeed: 'slow' // set to 0 to deactivate effect
     },
     settings = $.extend(defaults, options);
 
-    var headers = $('h1, h2, h3, h4, h5, h6').filter(function() {
+    var headers = $(settings.headers).filter(function() {
       // get all headers with an ID
       var previousSiblingName = $(this).prev().attr( "name" );
       if (!this.id && previousSiblingName) {
